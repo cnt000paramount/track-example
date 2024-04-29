7b3632946bbdcb0659610f94e87074fdda73d3f1
Edoardo Gargano <edoardo.gargano@paramount.com>
2024-04-29 17:29:33 +0200

    ttml2vtt

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function main(url) {
+function ttmlBlob(url) {
   fetch(url)
     .then((response) => response.text())
     // .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
@@ -14,7 +14,7 @@ function main(url) {
     });
 }
 
-function main2(url) {
+function vttSrc(url) {
   const track = document.createElement("track");
   track.src = url;
   track.kind = "captions";
@@ -24,5 +24,51 @@ function main2(url) {
   document.querySelector("video").appendChild(track);
 }
 
-// main("episode.ttml");
-// main2("subs.vtt");
+function vttBlob(url) {
+  fetch(url)
+    .then((response) => response.text())
+    .then((cc) => {
+      const blob = new Blob([cc], { type: "text/vtt" });
+      const track = document.createElement("track");
+      track.src = URL.createObjectURL(blob);
+      track.kind = "captions";
+      track.srclang = "en";
+      track.label = "English";
+      track.default = true;
+      document.querySelector("video").appendChild(track);
+    });
+}
+
+function ttml2Vttblob(url) {
+  fetch(url)
+    .then((response) => response.text())
+    .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then((cc) => {
+      const vtt = transformTTMLtoVTT(cc);
+      console.log(vtt);
+
+      const blob = new Blob([vtt], { type: "text/vtt" });
+      const track = document.createElement("track");
+      track.src = URL.createObjectURL(blob);
+      track.kind = "captions";
+      track.srclang = "en";
+      track.label = "English";
+      track.default = true;
+      document.querySelector("video").appendChild(track);
+    });
+}
+
+function transformTTMLtoVTT(ttml) {
+  let vtt = "";
+  ttml.querySelectorAll("tt body div p").forEach((p, i) => {
+    vtt += `${i + 1}
+
+${p.attributes.begin.value} --> ${p.attributes.end.value}
+‎${p.textContent}
+
+`;
+  });
+  return `WEBVTT
+
+${vtt}`;
+}
